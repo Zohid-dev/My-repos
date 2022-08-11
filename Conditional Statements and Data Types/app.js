4febd677ca1e295cc57c65b4e2ca0473464a1796
zohids <zohidjon.net@gmail.com>
2022-08-11 22:14:18 +0500

    3rd task

--- Conditional Statements and Data Types/app.js
+++ Conditional Statements and Data Types/app.js
@@ -27,3 +27,18 @@ console.log(strn * num); //returns 15 as a number is written as a string
 //number * boolean
 console.log(num * boolt); // returns 3 as true equals 1
 console.log(num * boolf); // returns 0 as false equals 0
+
+// 3. Divide different types (string / boolean, string / number, number / Boolean)
+
+//string / boolean
+console.log(str / boolt); //returns NaN
+console.log(strn / boolt); //returns 5 as true equals 1
+console.log(strn / boolf); //returns Infinity as false equals 0
+
+//string / number
+console.log(str / num); //returns NaN
+console.log(strn / num); //returns a number as a number is declared as string
+
+//number / Boolean
+console.log(num / boolt); //returns 3 as true equals 1
+console.log(num / boolf); //returns Infinity as false equals 0
