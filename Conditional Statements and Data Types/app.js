999b3c0a3173397bc3613c2f735937e54ebc35cd
zohids <zohidjon.net@gmail.com>
2022-08-11 22:01:56 +0500

    1st task

---
+++ Conditional Statements and Data Types/app.js
@@ -0,0 +1,19 @@
+// 1. Perform addition of various types (string + boolean, string + number, number + boolean)
+
+// string + boolean: 
+
+let str = "Hello"
+let boolt = true
+let boolf = false
+let num = 3
+
+// string + boolean: 
+console.log(str + boolt); //returns Hellotrue in console
+
+// string + number
+console.log(str + num); //returns Hello3
+
+// number + boolean
+console.log(num + boolt) //returns 4 as true equals 1
+console.log(num + boolf) //returns 3 as false equals 0
+
