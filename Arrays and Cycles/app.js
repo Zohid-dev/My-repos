a79713cd87898dfaa7ddb427d6aecbed2da0f841
zohid <zohidjon.net@gmail.com>
2022-08-27 19:16:49 +0500

    task1

---
+++ Arrays and Cycles/app.js
@@ -0,0 +1,22 @@
+//Task 1
+
+let arrMovies = ["Avengers", "The Net", "Matrix", "The Mask"]
+
+// for(let i = 0; i < arrMovies.length; i++){
+//     console.log(arrMovies[i]);
+// }
+
+// for (el of arrMovies){
+//     console.log(el);
+// }
+// let k = 0
+// while (k < arrMovies.length) {
+
+//     console.log(arrMovies[k]);
+//     k++
+// }
+
+do {    
+    console.log(arrMovies[k]);
+    k++
+} while (k < arrMovies.length);
