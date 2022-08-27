e2437553883b6b57a751ac2249f0e43dd589afdb
zohid <zohidjon.net@gmail.com>
2022-08-27 19:34:28 +0500

    task2

--- Arrays and Cycles/app.js
+++ Arrays and Cycles/app.js
@@ -16,7 +16,17 @@ let arrMovies = ["Avengers", "The Net", "Matrix", "The Mask"]
 //     k++
 // }
 
-do {    
-    console.log(arrMovies[k]);
-    k++
-} while (k < arrMovies.length);
+// do {    
+//     console.log(arrMovies[k]);
+//     k++
+// } while (k < arrMovies.length);
+
+//Task 2 Given an array of car manufacturers, convert the array to a string and back to an array
+
+let arrCars = ['Mercedes', 'BMW', 'Fiat']
+
+let allCars = arrCars.toString()
+
+let sameCars = allCars.split(",")
+
+console.log(sameCars);
