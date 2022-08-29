7529b0d7e3c02763cfe3a6929211fb78ce5010fe
zohid <zohidjon.net@gmail.com>
2022-08-29 10:42:29 +0500

    task3 date

--- Strings/app.js
+++ Strings/app.js
@@ -9,3 +9,10 @@ let num = '2 + 3 223 2223'
 
 let regexp = /2 \+ 3/
 console.log(num.match(regexp));
+
+//Task3
+let date = new Date
+let yy = date.getFullYear()
+let mm = date.getMonth() + 1
+let dd = date.getDate()
+console.log(`The year: ${yy}; The month: ${mm}; The day: ${dd}`);
