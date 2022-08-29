1027e4a62a584d9829e6aec301a24b7a67157fb7
zohid <zohidjon.net@gmail.com>
2022-08-29 08:39:51 +0500

    task2 ++

--- Strings/app.js
+++ Strings/app.js
@@ -3,3 +3,9 @@ let str = 'ahb acb aeb aeeb adcb axeb'
 
 let re = /a.b/g;
 console.log(str.match(re));
+
+//Task2
+let num = '2 + 3 223 2223'
+
+let regexp = /2 \+ 3/
+console.log(num.match(regexp));
